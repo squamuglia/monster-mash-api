95d0610e7efdcc749f1ad11190d88d2ea864490e
squamuglia <maxsmouha@gmail.com>
2018-07-27 16:14:02 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,25 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/hands', to: 'hands#index'
+      post '/hands', to: 'hands#create'
+      get '/hands/:id', to: 'hands#show'
+
+
       get '/heads', to: 'heads#index'
+      post '/heads', to: 'heads#create'
+      get '/heads/:id', to: 'heads#show'
+
+
       get '/feet', to: 'foots#index'
+      post '/feet', to: 'foots#create'
+      get '/feet/:id', to: 'foots#show'
+
+
       get '/users', to: 'users#index'
+      post '/users', to: 'users#create'
+      get '/users/:id', to: 'users#show'
+
+
 
     end
   end
